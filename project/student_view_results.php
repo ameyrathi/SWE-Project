fcfa45358478a28fc217b3c31ec7f8c2b8763016
ianliew2018 <54625604+ianliew2018@users.noreply.github.com>
2019-09-20 20:17:03 +0800

    created student's page for viewing results

---
+++ project/student_view_results.php
@@ -0,0 +1,19 @@
+<html>
+
+<link rel="stylesheet" href="stylesheet.css"/>
+
+<!-- The sidebar -->
+<div class="sidebar">
+  <a href="student_home.php">Home</a>
+  <a href="student_add_bid.php">Bid</a>
+  <a href="student_drop_bid.php">Drop Bid</a>
+  <a href="student_view_bid.php">View Bids</a>
+  <a class="active" href="student_view_results.php">View Results</a>
+  <a href="sign_out.php">Sign Out</a>
+</div>
+
+
+<div class="content">
+
+</div>
+</html>
