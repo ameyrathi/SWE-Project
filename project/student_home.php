669cdfb5e26115739df49fbb59ec9a2bfe3aabcd
ianliew2018 <54625604+ianliew2018@users.noreply.github.com>
2019-09-20 19:59:21 +0800

    Created student's home page

---
+++ project/student_home.php
@@ -0,0 +1,21 @@
+<html>
+
+<link rel="stylesheet" href="stylesheet.css"/>
+
+<!-- The sidebar -->
+<div class="sidebar">
+  <a class="active" href="student_home.php">Home</a>
+  <a href="student_add_bid.php">Bid</a>
+  <a href="student_drop_bid.php">Drop Bid</a>
+  <a href="student_view_bid.php">View Bids</a>
+  <a href="student_view_results.php">View Results</a>
+  <a href="sign_out.php">Sign Out</a>
+</div>
+
+<!-- Page content -->
+<div class="content">
+    <h1>Welcome to BIOS, {name}!</h1>
+    <h2>Your e$ balance is {balance}.</h2><br>
+</div>
+
+</html>
