61c6af5a6283122c03b42a8d6b1adc86e88782cb
charmainelrh <rouhui.lee.2018@sis.smu.edu.sg>
2019-09-25 15:45:56 +0800

    updated student_drop_bid.php

--- project/student_drop_bid.php
+++ project/student_drop_bid.php
@@ -2,13 +2,26 @@
 
 <link rel="stylesheet" href="stylesheet.css"/>
 
+<?php
+    require_once 'include/common.php';
+    require_once 'include/protect_token.php';
+
+    if(isset($_GET["token"])) {
+        $token = $_GET["token"];
+    } else {
+        $token = "";
+    }
+
+    token_gateway($token);
+?>
+
 <!-- The sidebar -->
 <div class="sidebar">
-  <a href="student_home.php">Home</a>
-  <a href="student_add_bid.php">Bid</a>
-  <a class="active" href="student_drop_bid.php">Drop Bid</a>
-  <a href="student_view_bid.php">View Bids</a>
-  <a href="student_view_results.php">View Results</a>
+  <a href="student_home.php?token=<?php echo $token?>">Home</a>
+  <a href="student_add_bid.php?token=<?php echo $token?>">Bid</a>
+  <a class="active" href="student_drop_bid.php?token=<?php echo $token?>">Drop Bid</a>
+  <a href="student_view_bid.php?token=<?php echo $token?>">View Bids</a>
+  <a href="student_view_results.php?token=<?php echo $token?>">View Results</a>
   <a href="sign_out.php">Sign Out</a>
 </div>
 
