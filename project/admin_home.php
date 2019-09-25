3a0f941c8ecf37e93df601a6fec065824062a001
Brian Goh <briangoh.junw@gmail.com>
2019-09-25 16:49:33 +0800

    Added admin_home.php
    Home page for admin. Need to do proper authentication for admin too (other pair)

---
+++ project/admin_home.php
@@ -0,0 +1,25 @@
+<?php
+    require_once "include/common.php";
+?>
+
+<html>
+
+<link rel="stylesheet" href="sidebar.css"/>
+
+<!-- The sidebar -->
+<div class="sidebar">
+  <a class="active" href="admin_home.php">Home</a>
+  <a href="admin_round.php">Round Management</a>
+  <a href="admin_bootstrap.php">Bootstrap</a>
+  <a href="sign_out.php">Sign Out</a>
+</div>
+
+
+<div class="content">
+<body>
+<p>
+<h1>Welcome, Admin!</h1>
+</p>
+</body>
+</div>
+</html>
