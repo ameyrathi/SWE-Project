5e8cd3aea75e1ffabaefebb5998ee5446962f955
Brian Goh <briangoh.junw@gmail.com>
2019-10-15 13:55:55 +0800

    Minor edit to bootstrap logic
    Admin is now able to conduct bootstrap no matter
    
    Rounds closing message for admin is also now displayed below the start/stop round button

--- app/admin_bootstrap.php
+++ app/admin_bootstrap.php
@@ -16,11 +16,13 @@
     require_once "bootstrap.php";
     $biddingrounddao = new BiddingRoundDAO();
 
-    $current_round = "TO CHANGE?";
+    $current_round = $biddingrounddao->get_current_round();
 
-    $round1_not_started = ($biddingrounddao->get_round() == 1) && ($biddingrounddao->get_status() == "Not Started");
-    
-
+    if($current_round == 0.5) {
+        $bootstrap_message = "Please conduct bootstrap to start Round 1 bidding.";
+    } else {
+        $bootstrap_message = "Please conduct bootstrap to restart bidding from Round 1.";
+    }
 ?>
 
 <!-- The sidebar -->
@@ -34,12 +36,23 @@
 <div class="content">
 
 <?php
-if($round1_not_started) { // if round 1 not started
-    if(isset($_FILES["bootstrap-file"])) { // round 1 not started and admin uploads bootstrap file
+    echo "
+    <h1>$bootstrap_message</h1><br>
+    <form method='post' enctype='multipart/form-data'>
+    File:
+    <input type='file' name='bootstrap-file' /><br><br>
+    <!-- substitute the above value with a valid token -->
+    <input type='submit' value='Bootstrap'/>
+    </form>
+    ";
+
+
+    if(isset($_FILES['bootstrap-file'])) {
         $bootstrap_success = doBootstrap();
 
-        if($bootstrap_success != "failed") {
-
+        if($bootstrap_success == "failed") {
+            echo "<span id='error'>Bootstrap failed. Please upload the correct zip file.</span>";
+        } else { // if bootstrap succeeded
             [$num_record_loaded, $errors] = $bootstrap_success;
 
             echo "<br><h1>Processed:</h1>";
@@ -59,29 +72,85 @@ if($round1_not_started) { // if round 1 not started
             }
         }
     }
-    
-    if(!isset($_FILES["bootstrap-file"]) || (isset($bootstrap_success) && $bootstrap_success == "failed")) {
-        
-        echo "
-        <h1>Upload a file to bootstrap and start Round 1 bidding:</h1><br>
-        <form method='post' enctype='multipart/form-data'>
-        File:
-        <input type='file' name='bootstrap-file' /><br><br>
-        <!-- substitute the above value with a valid token -->
-        <input type='submit' value='Bootstrap'/>
-        </form>
-        ";
 
-        if(isset($bootstrap_success) && $bootstrap_success == "failed") {
-            echo "<p id='error'>";
-            echo "Bootstrap failed. Please upload the correct zip file.";
-            echo "</p>";
-        }
-    }
+
+
+
+
+
+
+
+
+
+
+    // if(isset($_FILES["bootstrap-file"])) { // round 1 not started and admin uploads bootstrap file
+    //     $bootstrap_success = doBootstrap();
+
+    //     if($bootstrap_success != "failed") {
+
+    //         [$num_record_loaded, $errors] = $bootstrap_success;
+
+    //         echo "<br><h1>Processed:</h1>";
+
+            // foreach($num_record_loaded as $this_record_loaded) {
+            //     echo $this_record_loaded . "<br>";
+            // }
+
+            // echo "<br><h1>Errors:</h1>";
+
+            // if(empty($errors)) {
+            //     echo "No errors found.";
+            // } else {
+            //     foreach($errors as $error) {
+            //         echo $error . "<br>";
+            //     }
+            // }
+    //     }
+    // }
     
-} else {
-    echo "<h1>You have already completed bootstrapping.</h1>";
-}
+    // if(!isset($_FILES["bootstrap-file"]) || (isset($bootstrap_success) && $bootstrap_success == "failed")) {
+        
+    //     echo "
+    //     <h1>Upload a file to bootstrap and start Round 1 bidding:</h1><br>
+    //     <form method='post' enctype='multipart/form-data'>
+    //     File:
+    //     <input type='file' name='bootstrap-file' /><br><br>
+    //     <!-- substitute the above value with a valid token -->
+    //     <input type='submit' value='Bootstrap'/>
+    //     </form>
+    //     ";
+
+    //     if(isset($bootstrap_success) && $bootstrap_success == "failed") {
+    //         echo "<p id='error'>";
+    //         echo "Bootstrap failed. Please upload the correct zip file.";
+    //         echo "</p>";
+    //     }
+    // } else {
+    //     $bootstrap_success = doBootstrap();
+
+    //     if($bootstrap_success != "failed") {
+
+    //         [$num_record_loaded, $errors] = $bootstrap_success;
+
+    //         echo "<br><h1>Processed:</h1>";
+
+    //         foreach($num_record_loaded as $this_record_loaded) {
+    //             echo $this_record_loaded . "<br>";
+    //         }
+
+    //         echo "<br><h1>Errors:</h1>";
+
+    //         if(empty($errors)) {
+    //             echo "No errors found.";
+    //         } else {
+    //             foreach($errors as $error) {
+    //                 echo $error . "<br>";
+    //             }
+    //         }
+    //     }
+    // }
+
+
 
 ?>
 
