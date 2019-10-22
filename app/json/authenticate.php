dcb3977831be852daeb5d5b217571f9f23474264
blursotongkia <gordon.lim.2018@smu.edu.sg>
2019-10-22 14:29:42 +0800

    JSON debugs
    Authenticate debug and removal of displays

--- app/json/authenticate.php
+++ app/json/authenticate.php
@@ -3,39 +3,42 @@
     require_once '../include/token.php';
 
     // isMissingOrEmpty(...) is in common.php
-    $errors = [ isMissingOrEmpty ("username"), 
-    isMissingOrEmpty ("password") ];
+    $errors = [ isMissingOrEmpty ("username"), isMissingOrEmpty ("password") ];
     $errors = array_filter($errors);
 
-    if (!isEmpty($errors)) {
+    if(!isEmpty($errors)){
         $result = [
             "status" => "error",
             "message" => array_values($errors)
             ];
-    } else {
+    }
+    else
+    {
         $username = $_POST["username"];
         $password = $_POST["password"];
 
-        $StudentDAO = new StudentDAO();
+        $studentdao = new StudentDAO();
 
-        if(($StudentDAO->validUser($username)) == true) { // if username exists in database
-            if($StudentDAO->getPassword($username) == $password) { // if password is correct
+        if(strtolower($username) == "admin"){
+            if($studentdao->adminLogin($password) == $password){
                 $token = generate_token($username);
                 $result = [
                     "status" => "success",
                     "token" => $token
                 ];
-            } else { // if username is correct but password is wrong
+            }
+            else{
                 $result = [
                     "status" => "error",
-                    "message" => ["invalid username/password"]
-                    ];
+                    "message" => ["invalid password"]
+                ];
             }
-        } else { // if username not in database
+        }
+        else{
             $result = [
                 "status" => "error",
-                "message" => ["invalid username/password"]
-                ];
+                "message" => ["invalid username"]
+            ];
         }
     }
 
