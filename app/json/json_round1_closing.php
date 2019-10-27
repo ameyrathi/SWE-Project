0ec3786ff92c3a1da92c9c413d31df1327e382df
ianliew2018 <54625604+ianliew2018@users.noreply.github.com>
2019-10-27 20:09:16 +0800

    Debug JSON Round 1 Closing

--- app/json/json_round1_closing.php
+++ app/json/json_round1_closing.php
@@ -1,6 +1,6 @@
 <?php
 
-function close_bidding_round1(){
+function json_close_bidding_round1(){
     $biddao = new BidDAO();
     $sectiondao = new SectionDAO();
     $sectionresultsdao = new SectionResultsDAO();
