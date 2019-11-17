00aeb1ace5b163e6333ec5987da3d3a4b15ce910
Brian Goh <briangoh.junw@gmail.com>
2019-11-17 15:28:45 +0800

    Debug process_min_bid

--- app/json/update-bid.php
+++ app/json/update-bid.php
@@ -3,6 +3,7 @@
     require_once '../include/common.php';
     require_once '../include/token.php';
     require_once '../process_add_bid.php';
+    require_once 'json_process_min_bid.php';
     $sortclass = new Sort();
 
     // isMissingOrEmpty(...) is in common.php
@@ -340,110 +341,110 @@
     header('Content-Type: application/json');
     echo json_encode($result, JSON_PRETTY_PRINT);
     
-    function process_min_bid($course, $section) {
-        $successfuldao = new SuccessfulDAO();
-        $biddao = new BidDAO();
-        $sectionresultsdao = new SectionResultsDAO();
+    // function process_min_bid($course, $section) {
+    //     $successfuldao = new SuccessfulDAO();
+    //     $biddao = new BidDAO();
+    //     $sectionresultsdao = new SectionResultsDAO();
     
     
-        $current_min_bid = $sectionresultsdao->get_min_bid($course, $section);
-        $current_available_seats = $sectionresultsdao->get_available_seats($course, $section);
+    //     $current_min_bid = $sectionresultsdao->get_min_bid($course, $section);
+    //     $current_available_seats = $sectionresultsdao->get_available_seats($course, $section);
     
-        $round1_successful_bids = $successfuldao->retrieve_sort_this_section_bids($course, $section, 1);
-        $round2_pending_bids = $biddao->retrieve_sort_this_section_bids($course, $section, 2);
+    //     $round1_successful_bids = $successfuldao->retrieve_sort_this_section_bids($course, $section, 1);
+    //     $round2_pending_bids = $biddao->retrieve_sort_this_section_bids($course, $section, 2);
     
-        $num_round2_pending_bids = count($round2_pending_bids);
+    //     $num_round2_pending_bids = count($round2_pending_bids);
     
-        // echo "<h2>Round 2 list of bids:</h2>";
-        // var_dump($round2_pending_bids);
+    //     // echo "<h2>Round 2 list of bids:</h2>";
+    //     // var_dump($round2_pending_bids);
     
-        // echo "<br>";
+    //     // echo "<br>";
     
-        // echo "Round 2 bids: $num_round2_pending_bids<br>";
-        // echo "Round 2 available seats: $current_available_seats<br>";
+    //     // echo "Round 2 bids: $num_round2_pending_bids<br>";
+    //     // echo "Round 2 available seats: $current_available_seats<br>";
     
-        // Case 1: If there are less than N bids for the section (where N is the total available seats)
-        // The minimum bid value remains the same
-        if($num_round2_pending_bids < $current_available_seats) { // if num of round 2 bids < round 2 available seats
-            foreach($round2_pending_bids as [$this_userid, $this_amount]) {
-                $biddao->update_round2_bid_status($this_userid, $course, "Pending, successful");
-            }
+    //     // Case 1: If there are less than N bids for the section (where N is the total available seats)
+    //     // The minimum bid value remains the same
+    //     if($num_round2_pending_bids < $current_available_seats) { // if num of round 2 bids < round 2 available seats
+    //         foreach($round2_pending_bids as [$this_userid, $this_amount]) {
+    //             $biddao->update_round2_bid_status($this_userid, $course, "Pending, successful");
+    //         }
     
-            return $current_min_bid;
+    //         return $current_min_bid;
     
-        } elseif($num_round2_pending_bids == $current_available_seats) { // if num of round 2 bids = round 2 available seats
-            $clearing_price = $round2_pending_bids[$num_round2_pending_bids-1][1];
+    //     } elseif($num_round2_pending_bids == $current_available_seats) { // if num of round 2 bids = round 2 available seats
+    //         $clearing_price = $round2_pending_bids[$num_round2_pending_bids-1][1];
     
-            $num_clearing_price_bids = 0;
+    //         $num_clearing_price_bids = 0;
     
-            foreach($round2_pending_bids as [$this_userid, $this_amount]) {
-                if($this_amount == $clearing_price) {
-                    $num_clearing_price_bids++;
-                }
-                if($num_clearing_price_bids > 1) {
-                    break;
-                }
-            }
+    //         foreach($round2_pending_bids as [$this_userid, $this_amount]) {
+    //             if($this_amount == $clearing_price) {
+    //                 $num_clearing_price_bids++;
+    //             }
+    //             if($num_clearing_price_bids > 1) {
+    //                 break;
+    //             }
+    //         }
     
-            if($num_clearing_price_bids == 1) { // only 1 bid at clearing price, so all succeed
-                foreach($round2_pending_bids as [$this_userid, $this_amount]) {
-                    $biddao->update_round2_bid_status($this_userid, $course, "Pending, successful");
-                }
-            } else { // if more than 1 bid at clearing price, aka all clearing price bids fail
-                foreach($round2_pending_bids as [$this_userid, $this_amount]) {
-                    $biddao->update_round2_bid_status($this_userid, $course, "Pending, successful");
-                }
-            }
+    //         if($num_clearing_price_bids == 1) { // only 1 bid at clearing price, so all succeed
+    //             foreach($round2_pending_bids as [$this_userid, $this_amount]) {
+    //                 $biddao->update_round2_bid_status($this_userid, $course, "Pending, successful");
+    //             }
+    //         } else { // if more than 1 bid at clearing price, aka all clearing price bids fail
+    //             foreach($round2_pending_bids as [$this_userid, $this_amount]) {
+    //                 $biddao->update_round2_bid_status($this_userid, $course, "Pending, successful");
+    //             }
+    //         }
     
-            // return new minimum bid
-            if($current_min_bid < ($clearing_price+1)) {
-                $new_min_bid = $clearing_price + 1;
-                $sectionresultsdao->update_min_bid($course, $section, $new_min_bid);
-                return $new_min_bid;
-            } else {
-                return $current_min_bid;
-            }
+    //         // return new minimum bid
+    //         if($current_min_bid < ($clearing_price+1)) {
+    //             $new_min_bid = $clearing_price + 1;
+    //             $sectionresultsdao->update_min_bid($course, $section, $new_min_bid);
+    //             return $new_min_bid;
+    //         } else {
+    //             return $current_min_bid;
+    //         }
     
-        } else { // if num of round 2 bids > round 2 available seats
-            $clearing_price = $round2_pending_bids[$current_available_seats-1][1];
+    //     } else { // if num of round 2 bids > round 2 available seats
+    //         $clearing_price = $round2_pending_bids[$current_available_seats-1][1];
     
-            $num_clearing_price_bids = 0;
+    //         $num_clearing_price_bids = 0;
     
-            foreach($round2_pending_bids as [$this_userid, $this_amount]) {
-                if($this_amount == $clearing_price) {
-                    $num_clearing_price_bids++;
-                }
-                if($num_clearing_price_bids > 1) {
-                    break;
-                }
-            }
+    //         foreach($round2_pending_bids as [$this_userid, $this_amount]) {
+    //             if($this_amount == $clearing_price) {
+    //                 $num_clearing_price_bids++;
+    //             }
+    //             if($num_clearing_price_bids > 1) {
+    //                 break;
+    //             }
+    //         }
     
-            if($num_clearing_price_bids == 1) { // only 1 bid at clearing price, so all within capacity succeed
-                foreach($round2_pending_bids as [$this_userid, $this_amount]) {
-                    if($this_amount >= $clearing_price) {
-                        $biddao->update_round2_bid_status($this_userid, $course, "Pending, successful");
-                    } else {
-                        $biddao->update_round2_bid_status($this_userid, $course, "Pending, fail");
-                    }
-                }
-            } else { // if more than 1 bid at clearing price, aka all clearing price bids fail
-                foreach($round2_pending_bids as [$this_userid, $this_amount]) {
-                    if($this_amount > $clearing_price) {
-                        $biddao->update_round2_bid_status($this_userid, $course, "Pending, successful");
-                    } else {
-                        $biddao->update_round2_bid_status($this_userid, $course, "Pending, fail");
-                    }
-                }
-            }
+    //         if($num_clearing_price_bids == 1) { // only 1 bid at clearing price, so all within capacity succeed
+    //             foreach($round2_pending_bids as [$this_userid, $this_amount]) {
+    //                 if($this_amount >= $clearing_price) {
+    //                     $biddao->update_round2_bid_status($this_userid, $course, "Pending, successful");
+    //                 } else {
+    //                     $biddao->update_round2_bid_status($this_userid, $course, "Pending, fail");
+    //                 }
+    //             }
+    //         } else { // if more than 1 bid at clearing price, aka all clearing price bids fail
+    //             foreach($round2_pending_bids as [$this_userid, $this_amount]) {
+    //                 if($this_amount > $clearing_price) {
+    //                     $biddao->update_round2_bid_status($this_userid, $course, "Pending, successful");
+    //                 } else {
+    //                     $biddao->update_round2_bid_status($this_userid, $course, "Pending, fail");
+    //                 }
+    //             }
+    //         }
     
-            if($current_min_bid < ($clearing_price+1)) {
-                $new_min_bid = $clearing_price + 1;
-                $sectionresultsdao->update_min_bid($course, $section, $new_min_bid);
-                return $new_min_bid;
-            } else {
-                return $current_min_bid;
-            }
-        }
-    }
+    //         if($current_min_bid < ($clearing_price+1)) {
+    //             $new_min_bid = $clearing_price + 1;
+    //             $sectionresultsdao->update_min_bid($course, $section, $new_min_bid);
+    //             return $new_min_bid;
+    //         } else {
+    //             return $current_min_bid;
+    //         }
+    //     }
+    // }
 
 ?>
